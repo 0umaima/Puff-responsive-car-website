8d21ed9ef186610af9f4e757700879be5487b687
Aka-emmaa <Oumaimakhajaa@gmail.com>
2023-09-17 14:20:19 +0100

    Featured Car JS Mixitup filter

--- assets/js/main.js
+++ assets/js/main.js
@@ -42,11 +42,23 @@ window.addEventListener('scroll', scrollHeader)
 
 
 /*=============== MIXITUP FILTER FEATURED ===============*/
-
+let mixerFeatured = mixitup('.featured__content', {
+    selectors: {
+        target: '.featured__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
 
 /* Link active featured */ 
+const linkFeatured = document.querySelectorAll('.featured__item')
 
-
+function activeFeatured(){
+    linkFeatured.forEach(l=> l.classList.remove('active-featured'))
+    this.classList.add('active-featured')
+}
+linkFeatured.forEach(l=> l.addEventListener('click', activeFeatured))
 /*=============== SHOW SCROLL UP ===============*/ 
 function scrollUp(){
     const scrollUp = document.getElementById('scroll-up');
